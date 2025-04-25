64ed1654fed14103488146d1b1d9d5bdc06fc12e
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2025-04-25 21:23:08 +0200

    test

--- lusobites/src/App.js
+++ lusobites/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
+import RecipeDetail from './pages/RecipeDetail/RecipeDetail';
 // Importar outras páginas conforme necessário
 
 function App() {
@@ -13,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* Adicione outras rotas conforme necessário */}
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/category/:category" element={<Home />} />
+        <Route path="/country/:country" element={<Home />} />
+        <Route path="/login" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
